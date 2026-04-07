e66ed44d0c093245cc45aa391cf36255a807d4f1
VahithaGollapothu <23eg112d57@anurag.edu.in>
2026-04-07 15:33:59 +0530

    add category field to JobRequest

--- src/main/java/com/jobportal/dto/JobRequest.java
+++ src/main/java/com/jobportal/dto/JobRequest.java
@@ -1,5 +1,4 @@
 package com.jobportal.dto;
-
 import com.jobportal.model.Job;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -7,23 +6,17 @@ import lombok.Data;
 
 @Data
 public class JobRequest {
-
     @NotBlank(message = "Title is required")
     private String title;
-
     @NotBlank(message = "Description is required")
     private String description;
-
     @NotBlank(message = "Company is required")
     private String company;
-
     @NotBlank(message = "Location is required")
     private String location;
-
     private String salary;
-
     @NotNull(message = "Job type is required")
     private Job.JobType jobType;
-
     private Job.JobStatus status;
+    private String category;  // ← ADD THIS LINE
 }
